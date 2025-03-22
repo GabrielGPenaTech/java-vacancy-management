d6f5fe8edb14b12a149f352ce28101aac013ece7
GabrielGPenaTech <kbeca.gabriel@hotmail.com>
2025-03-22 07:04:31 -0300

    add annotations to entity

--- src/main/java/com/rseat/vacancy_management/modules/candidate/CandidateEntity.java
+++ src/main/java/com/rseat/vacancy_management/modules/candidate/CandidateEntity.java
@@ -1,15 +1,24 @@
 package com.rseat.vacancy_management.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID  id;
 
     @Pattern(regexp = "\\S+", message = "The field [name] should not be contain spaces")
@@ -24,5 +33,7 @@ public class CandidateEntity {
     private String description;
     private String curriculum;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
